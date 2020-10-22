1b0df911a895f1e9198eec0452e6493ad204fd7b
Nhi Nguyen <touchbar@android-e3ff92d36f6a4825.openwtdev>
2020-10-22 14:38:42 +0700

    config login routing

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+import {PLATFORM} from 'aurelia-pal';
 export class App {
-  public message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Bookstore';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      {route: '', moduleId: PLATFORM.moduleName('./login/login'), title: 'Login'},
+    ]);
+
+    this.router
+  }
 }
