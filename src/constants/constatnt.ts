69472d433155ef58ee31b72a9712ce8423e322ad
Nhi Nguyen <touchbar@android-e3ff92d36f6a4825.openwtdev>
2020-10-23 14:47:21 +0700

    install aurelia-validation

--- src/constants/constatnt.ts
+++ src/constants/constatnt.ts
@@ -1,4 +1,4 @@
 export class Constant {
     public baseUrl = `https://nga-book-api.herokuapp.com/api/`;
-    public authUrl = `${this.baseUrl}/auth/login`;
+    public authUrl = `${this.baseUrl}auths/login`;
 }
