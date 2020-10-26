22648a3e6d7e0a185b3f0b03798d8a68c200eec6
Nhi Nguyen <touchbar@android-e3ff92d36f6a4825.openwtdev>
2020-10-26 08:51:26 +0700

    login validate

--- src/modules/login/login.ts
+++ src/modules/login/login.ts
@@ -1,29 +1,39 @@
-import {inject} from 'aurelia-framework';
+import {inject, NewInstance} from 'aurelia-framework';
 import {AuthService} from '../../services/authService';
 import {Router} from 'aurelia-router';
+import {ValidationRules, ValidationController} from 'aurelia-validation';
 
-@inject(AuthService, Router)
+@inject(AuthService, Router, NewInstance.of(ValidationController))
 export class Login {
 
-	public username: string;
+  public username: string;
   public password: string;
 
-	constructor(
-		private authService: AuthService,
-		private router: Router,
-	) {
-	}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private controller: ValidationController
+  ) {
+    ValidationRules
+      .ensure((form: Login) => form.username).required().withMessage('Please enter your email')
+      .ensure((form: Login) => form.password).required().withMessage('Please enter your password')
+      .on(this);
+  }
 
-	login() {
-		this.authService.login(this.username, this.password)
-			.then((data: {token: string; message: string;}) => {
-				if (data.token) {
-					alert('Login success');
-					this.router.navigateToRoute('');
-				} else {
-					alert(data.message);
-				}
-			});
-	}
+  login() {
+    this.controller.validate().then(res => {
+      if (res.valid) {
+        this.authService.login(this.username, this.password)
+          .then((data: {token: string; message: string;}) => {
+            if (data.token) {
+              alert('Login success');
+              this.router.navigateToRoute('');
+            } else {
+              alert(data.message);
+            }
+          });
+      }
+    });
+  }
 
 }
