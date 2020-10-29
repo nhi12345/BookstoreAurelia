3ea02223dd1059d4828ecb5130aeac34b5f622b8
Nhi Nguyen <touchbar@android-e3ff92d36f6a4825.openwtdev>
2020-10-29 17:47:06 +0700

    validate create book

--- src/modules/new-book/new-book.ts
+++ src/modules/new-book/new-book.ts
@@ -1,10 +1,11 @@
-import { Router } from 'aurelia-router';
-import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {inject, NewInstance} from 'aurelia-framework';
 import './new-book.css'
 import {AuthorService} from 'services/authorService';
 import {Author, Book} from 'models/book/book';
 import {BookService} from 'services/bookService';
-@inject(AuthorService, BookService, Router)
+import {ValidationController, ValidationRules} from 'aurelia-validation';
+@inject(AuthorService, BookService, Router, NewInstance.of(ValidationController))
 export class NewBook {
 
   public authors: Author[] = [];
@@ -12,15 +13,22 @@ export class NewBook {
   public price: number = null;
   public description: string = '';
   public publisher: string = '';
-  public year:number = null;
+  public year: number = null;
   public cover: File = null;
-  public  authorSelected: Author;
+  public authorSelected: Author;
+  public book: Book;
 
   constructor(
     private authorService: AuthorService,
     private bookService: BookService,
     private router: Router,
+    private controller: ValidationController
   ) {
+    ValidationRules
+      .ensure((form: NewBook) => form.name).required().withMessage('Please enter book name')
+      .ensure((form: NewBook) => form.price).required().withMessage('Please enter book price')
+      .ensure((form: NewBook) => form.year).required().withMessage('Please enter the year of book production')
+      .on(this);
   }
 
   created() {
@@ -31,17 +39,22 @@ export class NewBook {
   }
 
   public submit() {
-    const reader = new FileReader();
-    reader.readAsDataURL(this.cover[0]);
-    reader.onload = () => {
-      let coverString = reader.result.toString().split(`,`)[1];
-      let newBook = new Book(this.name, this.description, parseFloat(this.price+''), parseInt(this.year+''), this.publisher, coverString, this.authorSelected);
-      this.bookService.createBook(newBook).then(() => {
-        alert('Success');
-        this.router.navigateToRoute('books');
-      });
-    };
-    // console.log(coverString);
+    this.controller.validate().then(res => {
+      console.log(res);
+      if (res.valid) {
+        const reader = new FileReader();
+        reader.readAsDataURL(this.cover[0]);
+        reader.onload = () => {
+          let coverString = reader.result.toString().split(`,`)[1];
+          let newBook = new Book(this.name, this.description, parseFloat(this.price + ''), parseInt(this.year + ''), this.publisher, coverString, this.authorSelected);
+          this.bookService.createBook(newBook).then((book) => {
+            this.book = book;
+            alert('Success');
+            this.router.navigateToRoute('books');
+          });
+        };
+      }
+    });
   }
 
 }
